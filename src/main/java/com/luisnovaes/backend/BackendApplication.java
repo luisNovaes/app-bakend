d1c48efb0803be3ce2ca7656a6002c90aa410e03
luisNovaes <luismagnovaes@gmail.com>
2020-11-26 09:21:25 -0300

    Produto e associação muito para muitos.

--- src/main/java/com/luisnovaes/backend/BackendApplication.java
+++ src/main/java/com/luisnovaes/backend/BackendApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.luisnovaes.backend.domain.Categoria;
+import com.luisnovaes.backend.domain.Produto;
 import com.luisnovaes.backend.repositorys.CategoriaRepository;
+import com.luisnovaes.backend.repositorys.ProdutoRepository;
 
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -26,7 +30,20 @@ public class BackendApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
