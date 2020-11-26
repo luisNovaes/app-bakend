3603b586b106ad59077f8d5722d5602f023ea129
luisNovaes <luismagnovaes@gmail.com>
2020-11-26 11:48:01 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/luisnovaes/backend/services/CategoriaService.java
+++ src/main/java/com/luisnovaes/backend/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.luisnovaes.backend.domain.Categoria;
 import com.luisnovaes.backend.repositorys.CategoriaRepository;
+import com.luisnovaes.backend.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
 }
